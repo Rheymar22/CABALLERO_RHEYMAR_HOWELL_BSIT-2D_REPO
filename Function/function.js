f6f4f6675c4fc026372394d6a7c1fa118315342c
caballerorheymar <caballerorheymar@gmail.com>
2025-03-22 17:59:04 +0800

    first commit

---
+++ Function/function.js
@@ -0,0 +1,31 @@
+var dogName = "rover"
+var dogWeight = 23;
+if (dogWeight > 20) {
+    console.log (dogName + "says WOOF WOOF");
+} else {
+    console.log (dogName + "says WOOF WOOF")
+}
+
+var dogName = "reneer"
+var dogWeight = 49;
+if (dogWeight > 20) {
+    console.log (dogName + "says WOOF WOOF");
+} else {
+    console.log (dogName + "says WOOF WOOF")
+}
+
+var dogName = "steven"
+var dogWeight = 40;
+if (dogWeight > 20) {
+    console.log (dogName + "says WOOF WOOF");
+} else {
+    console.log (dogName + "says WOOF WOOF")
+}
+
+var dogName = "bryan"
+var dogWeight = 30;
+if (dogWeight > 20) {
+    console.log (dogName + "says WOOF WOOF");
+} else {
+    console.log (dogName + "says WOOF WOOF")
+}
